451c0c23dd1e6a6ad2ddac388e5749d29af87b57
Tran Thi Nguyet Ha <hattn@vietis.com.vn>
2022-02-09 16:28:57 +0700

    Fix animation transition

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,27 +5,37 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
-    public float runSpeed = 100f;
+    public float runSpeed = 30f;
+    public Animator animator;
     float horizontalMove = 0f;
     bool jump = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("isJumping", true);
         }
     }
     void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.deltaTime, jump);
         jump = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
     }
 }
