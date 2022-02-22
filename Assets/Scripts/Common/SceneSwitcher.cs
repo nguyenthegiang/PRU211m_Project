eeac12f58ce5b6aba8b48c9f425ae8fd2294f14d
ChisKien <chiskien03214@gmail.com>
2022-02-22 22:57:06 +0700

    Update SceneSwitcher

--- Assets/Scripts/Common/SceneSwitcher.cs
+++ Assets/Scripts/Common/SceneSwitcher.cs
@@ -3,25 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-     static int _lastSceneIndex;
+     public static int _lastSceneIndex;
 
     public void loadSceneByIndex(int index)
     {
-        SceneManager.LoadScene(index);
+        SceneManager.LoadSceneAsync(index);
     }
 
     public void loadSceneByName(string name)
     {
-        SceneManager.LoadScene(name);
+        SceneManager.LoadSceneAsync(name);
     }
-    public static void goToGameOverScene() {
+    public static  void goToGameOverScene() {
         //store the index of last scene
         _lastSceneIndex = SceneManager.GetActiveScene().buildIndex;
         //load scene gameover
-        SceneManager.LoadScene("GameOver");
+        SceneManager.LoadSceneAsync("GameOver");
     }
 
     public static void restartLastScene() {
-        SceneManager.LoadScene(_lastSceneIndex);
+        SceneManager.LoadSceneAsync(_lastSceneIndex);
     }
 }
