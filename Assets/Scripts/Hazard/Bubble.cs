73b0606e31b6c2fe5e904db6bf9de8e12c4ea1a8
Tran Thi Nguyet Ha <hattnhe153222@fpt.edu.vn>
2022-02-24 09:37:39 +0700

    Fix bubble prefab script

--- Assets/Scripts/Hazard/Bubble.cs
+++ Assets/Scripts/Hazard/Bubble.cs
@@ -18,7 +18,7 @@ public class Bubble : Hazard
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Platformer")
+        if(collision.gameObject.tag == "Platformer" || collision.gameObject.tag == "Player")
         {
             Destroy(gameObject);
         }
