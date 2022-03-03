d2b1c62ea683f55eea555643e903a17e0ecf4f85
Tran Thi Nguyet Ha <hattnhe153222@fpt.edu.vn>
2022-03-03 09:08:50 +0700

    Add submarine bomb for underwater movement

--- Assets/Scripts/Player/UnderwaterMovement.cs
+++ Assets/Scripts/Player/UnderwaterMovement.cs
@@ -4,37 +4,50 @@ using UnityEngine;
 
 public class UnderwaterMovement : PlayerMovement
 {
-    public float speed = 5f;
+    public float speed = 3f;
+    [SerializeField]
+    GameObject submarineBomb;
 
-    float horizontalMove;
-    float verticalMove;
-    bool m_FacingRight = true;
+    float _horizontalMove;
+    float _verticalMove;
+    bool m_FacingRight = false;
     Rigidbody2D body;
-
+    Vector3 spawnPosition;
+    float objectHeight;
     // Start is called before the first frame update
     void Start()
     {
-        body = gameObject.GetComponent<Rigidbody2D>();        
+        body = gameObject.GetComponent<Rigidbody2D>();
+        objectHeight = gameObject.GetComponent<Renderer>().bounds.size.y;
+        spawnPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - objectHeight, gameObject.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxis("Horizontal");
-        verticalMove = Input.GetAxis("Vertical");
-        if (horizontalMove > 0 && !m_FacingRight)
+        _horizontalMove = Input.GetAxis("Horizontal");
+        _verticalMove = Input.GetAxis("Vertical");
+        if (_horizontalMove > 0 && !m_FacingRight)
         {
             // ... flip the player.
             Flip();
         }
         // Otherwise if the input is moving the player left and the player is facing right...
-        else if (horizontalMove < 0 && m_FacingRight)
+        else if (_horizontalMove < 0 && m_FacingRight)
         {
             // ... flip the player.
             Flip();
         }
-        Vector3 moveVector = new Vector3(horizontalMove, verticalMove, 0);
+        Vector3 moveVector = new Vector3(_horizontalMove, _verticalMove, 0);
         body.velocity = moveVector.normalized * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bomb = Instantiate(submarineBomb, spawnPosition, Quaternion.identity);
+            bomb.GetComponent<Rigidbody2D>().AddForce(2f * Vector3.down);
+        }
+        spawnPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - objectHeight, gameObject.transform.position.z);
+
     }
 
     void FixedUpdate()
