0b9be3b7376f5ff90730519421d19247af1a1f62
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2022-03-05 20:34:59 +0700

    Adjust RunningRock (Level2): Recreatable

--- Assets/Scripts/Hazard/RunningRock/RunningRock.cs
+++ Assets/Scripts/Hazard/RunningRock/RunningRock.cs
@@ -7,10 +7,25 @@ public class RunningRock : MonoBehaviour
     [SerializeField]
     public float thrust;
 
+    //for recreate rock
+    private GameObject RunningRockPrefab;
+
     void Start()
     {
+        RunningRockPrefab = (GameObject)Resources.Load(@"Prefabs\RunningRock");
+
         Rigidbody2D rd2d = gameObject.GetComponent<Rigidbody2D>();
         rd2d.AddForce(transform.right * thrust);
         rd2d.AddForce(transform.right * thrust, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //When kill mainCharacter -> Destroy & Recreate the Rock
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            Instantiate(RunningRockPrefab, new Vector3(13.54f, 0.69f), Quaternion.identity);
+        }
     }
 }
