67b18e96fc74dc468a92da828d4fe951c6081e97
Tran Thi Nguyet Ha <hattnhe153222@fpt.edu.vn>
2022-03-25 21:02:55 +0700

    Fix falling wall

--- Assets/Scripts/Hazard/FallingWall/FallingWall.cs
+++ Assets/Scripts/Hazard/FallingWall/FallingWall.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class FallingWall : MonoBehaviour
 {
     private GameObject fallingWall;
-
+    [SerializeField]
+    GameObject wallPivot;
+    [SerializeField]
+    GameObject[] spikes;
     Vector3 startPosition;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +17,7 @@ public class FallingWall : MonoBehaviour
 
 
         startPosition = transform.position;
+        GameObject trigger = GameObject.Find("fallingWallTrigger");
     }
 
     // Update is called once per frame
@@ -25,11 +29,25 @@ public class FallingWall : MonoBehaviour
             Destroy(gameObject);
 
             Instantiate(fallingWall, startPosition, Quaternion.identity);
-        }
-        GameObject trigger = GameObject.Find("fallingWallTrigger");
-        trigger.GetComponent<FallingWallTrigger>().spikes[0] = GameObject.Find("/FallingWall(Clone)/spike");
-        trigger.GetComponent<FallingWallTrigger>().spikes[1] = GameObject.Find("/FallingWall(Clone)/spike (1)");
+            GameObject trigger = GameObject.Find("fallingWallTrigger");
 
-        trigger.GetComponent<FallingWallTrigger>().isTriggered = false;
+            trigger.GetComponent<FallingWallTrigger>().isTriggered = false;
+
+        }
+
+        
+    }
+
+    public void RotateWall()
+    {
+        foreach (GameObject spike in spikes)
+        {
+            float spikewidth = spike.GetComponent<Renderer>().bounds.size.x;
+
+            Vector3 targetPosition = new Vector3(spike.transform.position.x + spikewidth / 2, spike.transform.position.y, spike.transform.position.z);
+            spike.transform.position = Vector3.MoveTowards(spike.transform.position, targetPosition, 1f);
+        }
+        gameObject.transform.RotateAround(wallPivot.transform.position, new Vector3(0, 0, 1), -20);
+
     }
 }
--- Assets/Scripts/Hazard/FallingWall/FallingWallTrigger.cs
+++ Assets/Scripts/Hazard/FallingWall/FallingWallTrigger.cs
@@ -4,58 +4,38 @@ using UnityEngine;
 
 public class FallingWallTrigger : MonoBehaviour
 {
-    public GameObject[] spikes;
+    //public GameObject[] spikes = new GameObject[2];
 
     Timer timer;
-    public bool isTriggered = false;
+    public bool isTriggered;
 
     private void Awake()
     {
-        
+        isTriggered = false;
     }
 
     private void Start()
     {
         timer = gameObject.AddComponent<Timer>();
         timer.Duration = 1;
-        spikes[0] = GameObject.Find("/FallingWall/spike");
-        spikes[1] = GameObject.Find("/FallingWall/spike (1)");
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
         if (collision.gameObject.tag == "Player" && isTriggered == false)
         {
-
-            StartCoroutine(moveSpike());
+            isTriggered = true;
 
             GameObject fallingWall = GameObject.Find("FallingWall");
             if (fallingWall == null)
             {
                 fallingWall = GameObject.Find("FallingWall(Clone)");
             }
-            GameObject wallPivot = GameObject.Find("FallingWall/wallPivot");
-            if (wallPivot == null)
-            {
-                wallPivot = GameObject.Find("FallingWall(Clone)/wallPivot");
-            }
-            Rigidbody2D rb = fallingWall.GetComponent<Rigidbody2D>();
-            fallingWall.transform.RotateAround(wallPivot.transform.position, new Vector3(0,0,1), -20);
-
-            isTriggered = true;
+            fallingWall.GetComponent<FallingWall>().RotateWall();
         }
         
     }
 
-    IEnumerator moveSpike()
-    {
-        foreach(GameObject spike in spikes)
-        {
-            float spikewidth = spike.GetComponent<Renderer>().bounds.size.x;
 
-            Vector3 targetPosition = new Vector3(spike.transform.position.x + spikewidth/2, spike.transform.position.y, spike.transform.position.z);
-            spike.transform.position = Vector3.MoveTowards(spike.transform.position, targetPosition, 1f);
-        }
-        yield return new WaitForSeconds(1);
-    }
 }
