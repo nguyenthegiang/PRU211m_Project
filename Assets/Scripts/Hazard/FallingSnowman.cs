bc23b6885fd08bfcb4871ea8e9437551e1fb8880
nguyenthegiang <nguyenthe.giang.7752@gmail.com>
2024-11-27 00:20:00 +0700

    feature/frozenLevel | Falling Snowman: reset Snowman position after hit

--- Assets/Scripts/Hazard/FallingSnowman.cs
+++ Assets/Scripts/Hazard/FallingSnowman.cs
@@ -1,9 +1,14 @@
+using System;
 using UnityEngine;
 
 public class FallingSnowman : MonoBehaviour
 {
     new Rigidbody2D rigidbody2D;
     bool fell = false;
+
+    // current position of Snowman (before game start)
+    public float posX;
+    public float posY;
 
     /// <summary>
     /// Sent when another object enters a trigger collider attached to this
@@ -17,21 +22,40 @@ public class FallingSnowman : MonoBehaviour
         {
             if (other.gameObject.tag == "Player")
             {
-                //change type body of rock to dynamic (can be effected by gravity)
+                //change type body of rock to dynamic (can be effected by gravity) -> Starts falling
                 rigidbody2D.isKinematic = false;
                 rigidbody2D.gravityScale = 4f;
                 fell = true;
             }
         }
     }
+
     void OnCollisionEnter2D(Collision2D other)
     {
-        //if rock touch platformer 
-        if (other.gameObject.tag == "Platformer")
+        switch (other.gameObject.tag)
         {
-            //change tag of rock from Hazard to Untagged
-            gameObject.tag = "Untagged";
+            case "Platformer":
+                {
+                    //if Snowman touch Ground
+                    gameObject.tag = "Untagged";
+                    break;
+                }
+            case "Player":
+                {
+                    // if Snowman hit Player -> Reset Position
+                    resetPosition();
+                    break;
+                }
         }
     }
 
+    private void resetPosition()
+    {
+        this.rigidbody2D.velocity = Vector2.zero;
+        this.rigidbody2D.angularVelocity = 0f;
+        this.rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+        this.gameObject.transform.position = new Vector3(posX, posY, 0);
+        this.gameObject.transform.rotation = Quaternion.identity;
+        fell = false;
+    }
 }
